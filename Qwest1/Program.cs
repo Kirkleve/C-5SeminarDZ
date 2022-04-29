8598a6d798d4dc2f3881d94b5f509badccf70132
Kirkleve <102994769+Kirkleve@users.noreply.github.com>
2022-04-30 00:05:40 +0200

    Fest qwest complite

---
+++ Qwest1/Program.cs
@@ -0,0 +1,34 @@
+﻿/*Задача 34: Задайте массив заполненный случайными положительными 
+трёхзначными числами. Напишите программу, которая покажет количество 
+чётных чисел в массиве.
+[345, 897, 568, 234] -> 2*/
+
+
+int [] random = new int[20];
+RandomArray(random);
+WriteArray(random);
+Console.WriteLine(random);
+int count = 0;
+for (int i = 0; i< random.Length; i++)
+{
+    if ( random[i] % 2 == 0 )
+    count++;
+}
+Console.WriteLine(count);
+
+void RandomArray(int[] array)
+{
+    for (int i = 0; i< array.Length; i++)
+    {
+        array[i] = new Random().Next(99,1000);
+    }
+}
+
+void WriteArray(int[] array)
+{
+    for (int i = 0; i< array.Length; i++)
+    {
+        Console.Write(array[i]+ " ");
+    }
+    Console.WriteLine();
+}
---
+++ Qwest2/Program.cs
@@ -0,0 +1,34 @@
+﻿/*Задача 36: Задайте одномерный массив, заполненный случайными числами. 
+Найдите сумму элементов, стоящих на нечётных позициях.
+[3, 7, 23, 12] -> 19
+[-4, -6, 89, 6] -> 0*/
+
+Console.WriteLine("Введите длину массива ");
+int Length = Convert.ToInt32(Console.ReadLine());
+int[] arrayMass = new int[Length];
+RandomArray(arrayMass);
+WriteArray(arrayMass);
+int count = 0;
+for (int i = 0; i< arrayMass.Length; i++)
+{
+    if ( arrayMass[i] % 2 == 0 )
+    count++;
+}
+Console.WriteLine(count);
+
+void RandomArray(int[] array)
+{
+    for (int i = 0; i< array.Length; i++)
+    {
+        array[i] = new Random().Next(99,1000);
+    }
+}
+
+void WriteArray(int[] array)
+{
+    for (int i = 0; i< array.Length; i++)
+    {
+        Console.Write(array[i]+ " ");
+    }
+    Console.WriteLine();
+}
